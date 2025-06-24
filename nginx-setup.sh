4b7f3e9003a3dae26c40d211b6250d4d0f968917
Leena <87615363+LeenaUkey@users.noreply.github.com>
2025-06-24 12:39:15 +0530

    code nginx-setup.sh

--- nginx-setup.sh
+++ nginx-setup.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+echo "Updating system..."
+sudo apt update -y
+
+echo "Installing NGINX..."
+sudo apt install nginx -y
+
+echo "Setting up index.html..."
+echo "Hello from Jenkins on EC2 with NGINX" | sudo tee /var/www/html/index.html
+
+echo "Starting and enabling NGINX..."
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+echo "Deployment complete. NGINX should be running on port 80."
 
